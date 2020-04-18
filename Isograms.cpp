6586db3f76df93f1aed6d33999ffec5d7107575c
Joshua G <gomesjoshua@gmail.com>
2020-04-18 09:31:06 -0700

    Update Isograms.cpp

--- Isograms.cpp
+++ Isograms.cpp
@@ -35,18 +35,24 @@ bool is_isogram(std::string str) {
 using namespace std;
 
 bool is_isogram(std::string str) {
-  bool res = true;
-  for (int i = 0; i < str.length() - 1; ++i) {
-    for (int j = i + 1; j < str.length(); ++j) {
-      if (tolower(str[i]) == tolower(str[j])) {
-          res = false;
+  bool res = true; // Variable to store result
+  
+  // Convert all chars to lower case
+  for (int i = 0; i < str.length(); i++) 
+        str[i] = tolower(str[i]);
+        
+  // Sort all chars in the string
+  sort(str.begin(), str.end());
+  
+  //One loop to check for repeating characters
+  for (int i = 0; i < str.length(); ++i) {
+      if (tolower(str[i]) == tolower(str[i + 1])) {
+          res = false; // if any matches, set res to false and break
           break;
           }
         }
-      }
    return res;
 }
-
 int main() {
   string a = "Dermatoglyphics";
   if (is_isogram(a))
