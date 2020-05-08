40bb3d396b8d76a3c08a1199772df2ef995302dc
Joshua G <gomesjoshua@gmail.com>
2020-05-08 16:23:54 -0700

    Update Rot13.cpp

--- Rot13.cpp
+++ Rot13.cpp
@@ -24,3 +24,42 @@ string rot13(string msg)
       
   return res;
 }
+// ######################################################################################
+/*
+Driver code
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string rot13(string msg)
+{
+  string res;
+  for(size_t i = 0; i < msg.length(); ++i) {
+    if (isalpha(msg[i])) {
+      char a = msg[i];
+      int b = int(a);
+      if (b < 78 || (b >= 97 && b <= 109)) {
+        a = a + 13;
+      }
+      else if (b >= 78 && b < 91) {
+        a = (b - 78) + 65;
+      }
+      else
+        a = (b - 110) + 97;
+      res.push_back(a);
+    }
+    else
+        res.push_back(msg[i]);
+        }
+      
+  return res;
+}
+
+int main() {
+  string s = "test";
+  cout << rot13(s) << endl;
+  return 0;
+}
+*/
