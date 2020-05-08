b997a09ce9b570c447f5789824e78214eca4e7ec
Joshua G <gomesjoshua@gmail.com>
2020-05-08 16:22:45 -0700

    Create Rot13.cpp

---
+++ Rot13.cpp
@@ -0,0 +1,26 @@
+#include <string>
+using namespace std;
+
+string rot13(string msg)
+{
+  string res;
+  for(size_t i = 0; i < msg.length(); ++i) {
+    if (isalpha(msg[i])) {
+      char a = msg[i];
+      int b = int(a);
+      if (b < 78 || (b >= 97 && b <= 109)) {
+        a = a + 13;
+      }
+      else if (b >= 78 && b < 91) {
+        a = (b - 78) + 65;
+      }
+      else
+        a = (b - 110) + 97;
+      res.push_back(a);
+    }
+    else
+        res.push_back(msg[i]);
+        }
+      
+  return res;
+}
